a416ae79a40ef6b8b7f4040424ef977996b28d88
jiashuai_shi <1712995121@qq.com>
2019-04-10 22:07:54 +0800

    修改选择器样式

--- pages/upload/index.js
+++ pages/upload/index.js
@@ -1,11 +1,23 @@
 // pages/upload/index.js
+const { $Toast } = require('../../dist/base/index');
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    citiesList: [],
+    citiesList: [
+      { pinyin: 'Beijing', name: '测试1' },
+      { pinyin: 'Beijing', name: '测试9' },
+      { pinyin: 'Beijing', name: '测试10' },
+      { pinyin: 'Shanghai', name: '测试2' },
+      { pinyin: 'Henan', name: '测试3' },
+      { pinyin: 'Aenan', name: '测试4' },
+      { pinyin: 'Denan', name: '测试5' },
+      { pinyin: 'Yenan', name: '测试6' },
+      { pinyin: 'Cenan', name: '测试7' },
+      { pinyin: 'Cenan', name: '测试8'},
+    ],
     cities: [],
     images: [],
     uploadNum: [0, 1, 2, 3],
@@ -23,7 +35,7 @@ Page({
       { ind: 2, status: '', name: '智能审核', desc: '智能审核' },
       { ind: 3, status: '', name: '人工复审', desc: '人工复审'},
     ],
-    judgeIsOpenIndexSelector: false
+    judgeIsOpenIndexSelector: true
   },
 
   /**
@@ -43,6 +55,7 @@ Page({
   // 初始化index选择器
   initIndexSel() {
     let storeCity = new Array(26);
+    console.log(storeCity)
     const words = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
     words.forEach((item, index) => {
       storeCity[index] = {
@@ -51,29 +64,27 @@ Page({
       }
     })
     this.data.citiesList.forEach((item) => {
+      console.log(item)
       let firstName = item.pinyin.substring(0, 1);
-      let index = words.indexOf(firstName);
+      console.log(firstName)
+      let index = words.indexOf(firstName.toUpperCase())
+      console.log(index)
       storeCity[index].list.push({
         name: item.name,
         key: firstName
       });
     })
-    this.data.cities = storeCity;
+    console.log(storeCity)
+    // this.data.cities = storeCity;
     this.setData({
-      cities: this.data.cities
+      cities: storeCity
     })
+    console.log(this.data.cities)
   },
   // index选择器触发
   onChange(event) {
     console.log(event)
     console.log(event.detail, 'click right menu callback data')
-    var obj1 = 'stepList[0].status'
-    var obj2 = 'stepList[1].status'
-    this.setData({
-      [obj1]: 'finish',
-      [obj2]: 'process',
-      judgeIsOpenIndexSelector: false
-    })
   },
   uploadImg(e) {
     console.log(e)
@@ -101,8 +112,34 @@ Page({
   // 选择品牌
   selectBrandBtn() {
     this.setData({
+      judgeIsOpenIndexSelector: false
+    })
+  },
+  selCurItem(e) {
+    console.log('点击了')
+    console.log(e)
+    this.setData({
+      'stepList[0].status': 'finish',
+      'stepList[1].status': 'process',
       judgeIsOpenIndexSelector: true
     })
+    console.log(this.data.stepList)
+  },
+  // 提交鉴定
+  submitAu() {
+    console.log(this.data.images.length)
+    if (this.data.images.length != 4) {
+      $Toast({
+        content: '请上传必须图片',
+        type: 'error'
+      });
+      return
+    } else {
+      this.setData({
+        'stepList[1].status': 'finish',
+        'stepList[2].status': 'process'
+      })
+    }
   },
   /**
    * 生命周期函数--监听页面显示
